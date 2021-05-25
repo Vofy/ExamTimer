5286256ad86f255685b17f6461537f763e2099ef
Tomáš Batelka <tomas.batelka@email.cz>
2021-05-25 14:03:37 +0200

    Initial commit

---
+++ ExamTimer/ExamTimer/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExamTimer
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static System.Timers.Timer timer;
+        private Part[] examParts;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            examParts = new Part[] {
+                new Part("Introduction", 1000),
+                new Part("Part 1", 4000),
+                new Part("Part 2", 4000, new Part[] {
+                    new Part("Subpart 1", 2000),
+                    new Part("Subpart 2", 2000)}),
+                new Part("Part 3", 6000, new Part[] {
+                    new Part("Subpart 1", 3000),
+                    new Part("Subpart 2", 3000)}),
+            };
+
+            // Nastavení intervalu na 1 sekundu
+            timer = new System.Timers.Timer(1000);
+
+            // Přiřazení události (která se provede vždy na konci periody
+            timer.Elapsed += Timer_Elapsed;
+
+            // Start the timer
+            timer.Enabled = true;
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ExamTimer/ExamTimer/Part.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamTimer
+{
+    class Part
+    {
+        private string name;
+        private int time;
+        private Part[] subparts;
+
+        public Part(string name, int time)
+        {
+            this.name = name;
+            this.time = time;
+        }
+
+        public Part(string name, int time, Part[] subparts)
+        {
+            this.name = name;
+            this.time = time;
+            this.subparts = subparts;
+        }
+
+        public string Name { get => name; }
+        public int Time { get => time; }
+        internal Part[] Subparts { get => subparts; }
+    }
+}
